e859f3bd22ee024915bb2c4cfcc041ee3f9a632d
chuch <eajebon@gmail.com>
2021-04-10 12:05:50 +0100

    Update BudgetApp.py
    fixed individual balances

--- BudgetApp.py
+++ BudgetApp.py
@@ -8,30 +8,31 @@ class budget:
 # 4.  Transferring balance amounts between categories
 
 # Push your code to GitHub, and submit the repo link.
-    database = {}
+    # database = {}
 
     def __init__(self, name):
         self.name = input("what would you like to budget on: ")
         self.balance = 0
         self.expenditure = 0
+        self.database = {}
         
 
     def deposit(self):
-        deposit_amount = int(input("enter deposit amount: "))
+        deposit_amount = int(input("enter deposit amount %s: " % self.name))
         self.balance += deposit_amount
         print("%d deposited of for %s" %(self.balance, self.name))
-        budget.database[self.name]= [deposit_amount]
-        print(budget.database)
+        self.database[self.name]= [deposit_amount]
+        print(self.database)
 
     def withdraw(self):
-        amount = int(input("how much do you wanna withdraw: "))
-        for self.name,details in budget.database.items():
+        amount = int(input("how much do you wanna withdraw %s: " % self.name))
+        for self.name,details in self.database.items():
             if details[0] > amount:
                 #if amount > 100:
                     self.balance -= amount
                     self.expenditure += amount
-                    budget.database[self.name]= [self.balance]
-                    print(budget.database)
+                    self.database[self.name]= [self.balance]
+                    print(self.database)
                     print("after %d expenditure on %s, %d balance is left" %(self.expenditure,self.name,self.balance))
                    
                 #else:
@@ -41,21 +42,26 @@ class budget:
 
 
     def category_balance(self):
-         print ("balance is %s " % budget.database[self.name])
+         print ("balance for %s is %s " %(self.name, self.database[self.name]))
          #print(budget.database)
 
     def balance_transfer(self):
         print("enter transfer details")
         
+        
 
-    def decison(sef):
+    def decison(self):
         print("what other option would you like to do (1) deposit (2) ")
         print
 
 food1 = budget("food")
-food2 = budget("food")
+food2 = budget("eba")
 
 food1.deposit()
 food2.deposit()
-#food1.withdraw()
-#food1.category_balance()
+
+food1.withdraw()
+food2.withdraw()
+
+food1.category_balance()
+food2.category_balance()
