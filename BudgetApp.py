1de49ce9d7f8cf602c0f6bc97d8197b713f0b744
chuch <eajebon@gmail.com>
2021-04-13 18:01:33 +0100

    Update BudgetApp.py
    added more functionality to the transfer method

--- BudgetApp.py
+++ BudgetApp.py
@@ -1,44 +1,44 @@
-#import randrange
+database = {}
 class budget:
 
-    # database = {}
+    
 
     def __init__(self, name):
         self.name = name
         self.balance = 0
         self.expenditure = 0
-        self.database = {}
+        #self.database = {}
         
 
     def deposit(self):
         deposit_amount = int(input("enter deposit amount %s: " % self.name))
         self.balance += deposit_amount
         print("%d deposited of for %s" %(self.balance, self.name))
-        self.database[self.name]= deposit_amount
-        print(self.database)
+        database[self.name]= deposit_amount
+        print(database)
 
     def withdraw(self):
         amount = int(input("how much do you wanna withdraw from %s: " % self.name))
-        if self.database[self.name] >= amount:
+        if database[self.name] >= amount:
             if amount not in range(0,999):
                 self.balance -= amount
                 self.expenditure += amount
-                self.database[self.name]= self.balance
+                database[self.name]= self.balance
                 #self.database[self.name] = self.database[self.name] - 500
                     
 
-                print(self.database)
+                print(database)
                 print("after %d expenditure on %s, %d balance is left" %(self.expenditure,self.name,self.balance))
                    
             else:
                 print("only multiples of 1000 is allowed to be withdrawn")
-                budget1.withdraw()
+                food.withdraw()
         else:
             print("insufficeint balance")
-            budget1.withdraw()
+            food.withdraw()
 
     def category_balance(self):
-         print ("balance for %s is %s " %(self.name, self.database[self.name]))
+         print ("balance for %s is %s " %(self.name, database[self.name]))
          
 
 
@@ -51,18 +51,33 @@ class budget:
 
     def balance_transfer(self,category_to_transfer):
         self.category_to_transfer = category_to_transfer 
-        amount_to_transfer = int(input("enter amount to transfer: "))
+        if category_to_transfer in database: 
+            amount_to_transfer = int(input("enter amount to transfer: "))
+            #for self.name,details in self.database.items():
+            if amount_to_transfer <= database[self.name] :
+                updated_old_balance = database[self.name] - amount_to_transfer
+                database[self.name] = updated_old_balance
+                #budget.budget1 = budget(self.category_to_transfer)
+                
+                #self.database [category_to_transfer]= amount_to_transfer
+                database [self.category_to_transfer]+= amount_to_transfer
+                print(database)
+
+            else:
+                print("amount to be withdraw is greater than your balance")
+
+
+        else:
+            amount_to_transfer = int(input("enter amount to transfer: "))
         #for self.name,details in self.database.items():
-        if amount_to_transfer <= self.database[self.name] :
-            updated_old_balance = self.database[self.name] - amount_to_transfer
-            self.database[self.name] = updated_old_balance
+            if amount_to_transfer <= database[self.name] :
+                updated_old_balance = database[self.name] - amount_to_transfer
+                database[self.name] = updated_old_balance
             #budget.budget1 = budget(self.category_to_transfer)
             
             #self.database [category_to_transfer]= amount_to_transfer
-            self.database [self.category_to_transfer]= amount_to_transfer
-            print(self.database)
-
-            
+                database [self.category_to_transfer]= amount_to_transfer
+                print(database)    
                 # alien_0['x_position'] = alien_0['x_position'] + x_increment
                 # print(f"New position: {alien_0['x_position']}")
                 # print(alien_0)
@@ -75,8 +90,8 @@ class budget:
             
             
             
-        else:
-            print("amount to be withdraw is greater than your balance")
+            else:
+                print("amount to be withdraw is greater than your balance")
 
         
         
@@ -86,19 +101,19 @@ class budget:
 
         print
 
-budget1 = budget("food")
-# budget2 = budget("eba")
+food = budget("food")
+entertainment = budget("eba")
 #budget2 = budget("")
 
-budget1.deposit()
-#budget2.deposit()
+food.deposit()
+entertainment.deposit()
 
-budget1.withdraw()
-#budget2.withdraw()
+food.withdraw()
+entertainment.withdraw()
 
-budget1.balance_transfer(input("what category do u want to transfer to: "))
+food.balance_transfer(input("what category do u want to transfer to: "))
 
-budget1.category_balance()
-#budget2.category_balance()
+food.category_balance()
+entertainment.category_balance()
 
 
